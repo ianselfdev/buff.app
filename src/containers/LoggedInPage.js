bfc687bdcc19c20dd319f046b0551d446afdb34b
roquefore <roquefore@gmail.com>
2018-12-28 15:42:49 +0200

    [update] registration page update

--- src/containers/LoggedInPage.js
+++ src/containers/LoggedInPage.js
@@ -1,20 +1,23 @@
 import { connect } from 'react-redux';
 import ApBarDashboard from '../components/ApBarDashboard/AppBarDashboard';
-import {bindActionCreators} from 'redux';
+import { bindActionCreators } from 'redux';
 import * as mainActions from '../actions/mainActions';
-console.log('TodoActionCreators',mainActions);
-const mapStateToProps = (state,ownProps) => ({
-  allHistory:state.reducerMain.allHistory,
-  username:state.reducerMain.username,
-  ownProps:ownProps,
-  tournaments:state.reducerMain.allTournaments,
-  address:state.reducerMain.address
+// console.log('TodoActionCreators',mainActions);
+const mapStateToProps = (state, ownProps) => ({
+    allHistory: state.reducerMain.allHistory,
+    username: state.reducerMain.username,
+    ownProps: ownProps,
+    tournaments: state.reducerMain.allTournaments,
+    address: state.reducerMain.address,
 });
 
 function mapDispatchToProps(dispatch) {
-  return {
-    ...bindActionCreators(mainActions, dispatch)
-  }
+    return {
+        ...bindActionCreators(mainActions, dispatch),
+    };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ApBarDashboard);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(ApBarDashboard);
