021982ff362ef5b91231d5023c798468f62af035
roquefore <roquefore@gmail.com>
2019-01-20 13:16:04 +0200

    basic items + layout

--- src/components/MarketItem/index.js
+++ src/components/MarketItem/index.js
@@ -8,7 +8,13 @@ export default class MarketItem extends Component {
     render() {
         return (
             <div className={Styles.container}>
-                <div className={Styles.title}>itemname</div>
+                <div className={Styles.title}>
+                    itemname
+                    <p className={Styles.description}>
+                        Detailed description. Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
                 <div className={Styles.actionButtonContainer}>
                     <p>500₿</p>
                     <p>BUY</p>
