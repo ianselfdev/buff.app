6b78b9d940dbe5e1808a7024d128beca3f9021de
roquefore <roquefore@gmail.com>
2019-03-30 02:32:41 +0200

    daily bonus mechanism

--- src/components/DailyBonus/index.js
+++ src/components/DailyBonus/index.js
@@ -6,46 +6,103 @@ import { connect } from 'react-redux';
 import Styles from './styles.module.scss';
 
 //Instuments
-import coin from '../../theme/svg/coin.svg';
+// import coin from '../../theme/svg/coin.svg';
 
 //Actions
 import { bonusesActions } from '../../bus/app/bonuses/actions';
+import { historyActions } from '../../bus/app/history/actions';
 
 const mapStateToProps = (state) => ({
     bonuses: state.bonuses,
+    history: state.history,
 });
+//40b5c672-9c78-4c01-9baf-1d9aa71919b7
 
 const mapDispatchToProps = {
     fetchAvailableBonusesAsync: bonusesActions.fetchAvailableBonusesAsync,
+    activateBonus: bonusesActions.activateBonus,
+    fetchHistoryAsync: historyActions.fetchHistoryAsync,
 };
 
 class DailyBonus extends Component {
     componentDidMount = () => {
-        const { fetchAvailableBonusesAsync } = this.props;
+        const { fetchAvailableBonusesAsync, fetchHistoryAsync } = this.props;
 
+        fetchHistoryAsync();
         fetchAvailableBonusesAsync();
+
+        setInterval(() => {
+            this.forceUpdate();
+        }, 1000);
+    };
+
+    _calculateTimeToTheNextDailyBonus = () => {
+        const { history } = this.props;
+
+        const lastDailyBonusTransaction = history
+            .get('history')
+            .find((item) => item.get('data').get('id') === '40b5c672-9c78-4c01-9baf-1d9aa71919b7');
+
+        const nextDailyBonusDate = lastDailyBonusTransaction
+            ? new Date(lastDailyBonusTransaction.get('createdAt')).getTime() + 60 * 60 * 24 * 1000
+            : null;
+
+        const timeToNextDailyBonus = nextDailyBonusDate - Date.now();
+
+        return Math.max(Math.round(timeToNextDailyBonus / 1000), 0);
+    };
+
+    _activateBonus = () => {
+        const { activateBonus, bonuses } = this.props;
+
+        const dailyBonus = bonuses.find(
+            (item) => item.get('data').get('id') === '40b5c672-9c78-4c01-9baf-1d9aa71919b7',
+        );
+
+        if (dailyBonus) {
+            activateBonus(dailyBonus);
+        } else {
+            return null;
+        }
     };
 
     render() {
+        const timeToTheNextBonus = this._calculateTimeToTheNextDailyBonus();
+
         return (
             <div className={Styles.container}>
                 <p className={Styles.text}>You may get your next bonus after:</p>
                 <div className={Styles.timer}>
                     <div className={Styles.timerItem}>
-                        <p className={Styles.timerNumber}>12</p>
+                        <p className={Styles.timerNumber}>
+                            {Math.floor(timeToTheNextBonus / 3600)
+                                .toString()
+                                .padStart(2, 0)}
+                        </p>
                     </div>
                     <span>:</span>
                     <div className={Styles.timerItem}>
-                        <p className={Styles.timerNumber}>12</p>
+                        <p className={Styles.timerNumber}>
+                            {Math.floor(timeToTheNextBonus / 3600 / 60)
+                                .toString()
+                                .padStart(2, 0)}
+                        </p>
                     </div>
                     <span>:</span>
                     <div className={Styles.timerItem}>
-                        <p className={Styles.timerNumber}>12</p>
+                        <p className={Styles.timerNumber}>
+                            {(timeToTheNextBonus % 3600).toString().padStart(2, 0)}
+                        </p>
                     </div>
                 </div>
-                <button className={Styles.button}>
-                    <img src={coin} alt="" />
-                    10
+                <button
+                    className={Styles.button}
+                    disabled={timeToTheNextBonus !== 0}
+                    onClick={this._activateBonus}
+                >
+                    {/* <img src={coin} alt="" />
+                    10 */}
+                    Get bonus
                 </button>
             </div>
         );
