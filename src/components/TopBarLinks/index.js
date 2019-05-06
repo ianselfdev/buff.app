eeb98eb138b0329456b64cf540e448dec109220d
roquefore <roquefore@gmail.com>
2019-05-07 00:25:14 +0300

    [fix] BUFF-334

--- src/components/TopBarLinks/index.js
+++ src/components/TopBarLinks/index.js
@@ -8,13 +8,31 @@ export default class TopBarLinks extends Component {
     render() {
         return (
             <div className={Styles.container}>
-                <a className={Styles.link} href="https://buff.game" alt="">
+                <a
+                    className={Styles.link}
+                    href="https://buff.game/faq/"
+                    alt=""
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     FAQ
                 </a>
-                <a className={Styles.link} href="https://buff.game" alt="">
+                <a
+                    className={Styles.link}
+                    href="https://forms.gle/M9if7dJ2AnqGFjdaA"
+                    alt=""
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     Report a bug
                 </a>
-                <a className={Styles.link} href="https://buff.game" alt="">
+                <a
+                    className={Styles.link}
+                    href="https://buff.game/contact-us/"
+                    alt=""
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     Contact us
                 </a>
             </div>
